0dd004412017636b04dc69ffd7e01bdb6e093524
Yuri Na <soceu@naver.com>
2025-01-12 22:37:25 +0900

    Feat: canvas 차트 그리기 연습

--- src/components/ChangeGraph.vue
+++ src/components/ChangeGraph.vue
@@ -1,47 +1,60 @@
 <template>
    <div>
-      <canvas
-         ref="canvasRef"
-         :width="canvasWidth"
-         :height="canvasHeight"
-      ></canvas>
+      <canvas ref="canvasRef" width="400px" height="300px"></canvas>
    </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
+import { LineChart } from '../utils/graph/changeGraph';
 
 const canvasRef = ref<HTMLCanvasElement | null>(null);
-const canvasWidth = ref(800);
-const canvasHeight = ref(600);
+// const canvasWidth = ref(800);
+// const canvasHeight = ref(600);
 
 onMounted(() => {
-   setCanvasSize();
+   // setCanvasSize();
+   renderCanvas();
 });
 
-const setCanvasSize = () => {
-   const screenWidth = window.innerWidth;
-   const screenHeight = window.innerHeight;
+// const setCanvasSize = () => {
+//    const screenWidth = window.innerWidth;
+//    const screenHeight = window.innerHeight;
 
-   // 원본 이미지 크기
-   const originalWidth = 7;
-   const originalHeight = 3;
-   const ratio = originalWidth / originalHeight;
+//    // 원본 이미지 크기
+//    const originalWidth = 7;
+//    const originalHeight = 3;
+//    const ratio = originalWidth / originalHeight;
 
-   // 1) 우선 가로폭에 맞추어 설정
-   let newWidth = screenWidth;
-   let newHeight = screenWidth / ratio;
+//    // 1) 우선 가로폭에 맞추어 설정
+//    let newWidth = screenWidth;
+//    let newHeight = screenWidth / ratio;
 
-   // 2) 만약 세로가 화면 높이를 넘어간다면, 세로를 기준으로 맞춤
-   if (newHeight > screenHeight) {
-      newHeight = screenHeight;
-      newWidth = screenHeight * ratio;
-   }
+//    // 2) 만약 세로가 화면 높이를 넘어간다면, 세로를 기준으로 맞춤
+//    if (newHeight > screenHeight) {
+//       newHeight = screenHeight;
+//       newWidth = screenHeight * ratio;
+//    }
 
-   // 3) 최종 계산된 값을 캔버스 크기로 설정
-   canvasWidth.value = newWidth;
-   canvasHeight.value = newHeight;
+//    // 3) 최종 계산된 값을 캔버스 크기로 설정
+//    canvasWidth.value = newWidth;
+//    canvasHeight.value = newHeight;
+// };
+
+const renderCanvas = () => {
+   const canvas = canvasRef.value;
+   if (!canvas) return;
+
+   const ctx = canvas.getContext('2d');
+   if (!ctx) return;
+
+   new LineChart(canvas, ctx);
+   // drawChart(ctx);
 };
+
+// const drawChart = ctx => {
+//    ctx.fillRect(25, 25, 100, 100);
+// };
 </script>
 
 <style scoped></style>
---
+++ src/utils/graph/changeGraph.ts
@@ -0,0 +1,101 @@
+const XAXIS_PADDING = 15;
+const YAXIS_PADDING = 25;
+const DURATION = 1000 * 30; // 30s
+const MAX_VALUE = 100;
+const Y_TICK_COUNT = 5;
+const TOP_PADDING = 15;
+const EX_TEXT = '00:00';
+
+export class LineChart {
+   canvas: HTMLCanvasElement;
+   ctx: CanvasRenderingContext2D;
+   canvasWidth: number;
+   canvasHeight: number;
+   chartWidth: number;
+   chartHeight: number;
+   xFormatWidth: number;
+   startTime: number;
+   endTime: number;
+   xTimeInterval: number;
+
+   constructor(ref: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+      this.canvas = ref;
+      this.ctx = ctx;
+
+      this.canvasWidth = ref.width; // 실제 픽셀 크기
+      this.canvasHeight = ref.height;
+      this.chartWidth = this.canvasWidth - YAXIS_PADDING;
+      this.chartHeight = this.canvasHeight - XAXIS_PADDING - TOP_PADDING;
+
+      this.xFormatWidth = this.ctx.measureText(EX_TEXT).width;
+      this.startTime = 0;
+      this.endTime = 0;
+      this.xTimeInterval = 0;
+
+      this.setTime();
+      this.drawChart();
+   }
+
+   // 시간을 실시간으로 세팅
+   setTime = () => {
+      this.endTime = Date.now();
+      this.startTime = this.endTime - DURATION;
+      this.setXInterval();
+   };
+
+   setXInterval = () => {
+      let xPoint = 0;
+      let timeInterval = 1000;
+      while (true) {
+         xPoint = (timeInterval / DURATION) * this.chartWidth;
+         if (xPoint > this.xFormatWidth) {
+            break;
+         }
+         timeInterval *= 2;
+      }
+
+      this.xTimeInterval = timeInterval;
+   };
+
+   // 차트를 그리는 함수
+   drawChart = () => {
+      const { ctx, chartHeight, canvasWidth, canvasHeight } = this;
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight); // 캔버스 초기화
+
+      ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight); // 캔버스 초기화
+
+      ctx.beginPath();
+      ctx.moveTo(YAXIS_PADDING, TOP_PADDING);
+      // draw YAXIS
+      ctx.lineTo(YAXIS_PADDING, chartHeight + TOP_PADDING);
+
+      const yInterval = MAX_VALUE / (Y_TICK_COUNT - 1);
+      ctx.textAlign = 'right';
+      ctx.textBaseline = 'middle';
+      for (let i = 0; i < Y_TICK_COUNT; i++) {
+         const value = i * yInterval;
+         const yPoint =
+            TOP_PADDING + chartHeight - (value / MAX_VALUE) * chartHeight;
+         ctx.fillText(value.toString(), YAXIS_PADDING - 3, yPoint);
+      }
+      // draw XAXIS
+      ctx.lineTo(canvasWidth, this.chartHeight + TOP_PADDING);
+      ctx.stroke();
+
+      let currentTime = this.startTime - (this.startTime % this.xTimeInterval);
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'top';
+      while (currentTime < this.endTime + this.xTimeInterval) {
+         const xPoint =
+            ((currentTime - this.startTime) / DURATION) * this.chartWidth;
+         const date = new Date(currentTime);
+         const text = date.getMinutes() + ':' + date.getSeconds();
+
+         ctx.fillText(text, xPoint, chartHeight + TOP_PADDING + 4);
+         currentTime += this.xTimeInterval;
+      }
+   };
+
+   // 데이터를 갱신하는 함수
+   updateData = () => {};
+}
